4d8e561e4c8be0f0e9e0f1d369907110870a810c
albertliu <xiaochuan_liu@hotmail.com>
2020-06-10 17:02:58 +0800

    format

--- studentNeedDiplomaListIncDetail.js
+++ studentNeedDiplomaListIncDetail.js
@@ -3,8 +3,8 @@
 						<form><label>搜索:</label>
 				          <input type="text" id="txtSearchStudentNeedDiploma" name="txtSearchStudentNeedDiploma" size="14" title="姓名、身份证、证书名称" style="background:yellow;" />
 				          <input class="button" type="button" id="btnSearchStudentNeedDiploma" value="查找">
-					          &nbsp;&nbsp;公司&nbsp;<select id="searchStudentNeedDiplomaHost" style="width:100px"></select>&nbsp;&nbsp;
-					          证书&nbsp;<select id="searchStudentNeedDiplomaCert" style="width:80px"></select>&nbsp;&nbsp;
+					          &nbsp;&nbsp;公司&nbsp;<select id="searchStudentNeedDiplomaHost" style="width:150px"></select>&nbsp;&nbsp;
+					          证书&nbsp;<select id="searchStudentNeedDiplomaCert" style="width:120px"></select>&nbsp;&nbsp;
 		          			<input style="border:0px;" type="checkbox" id="searchStudentNeedDiplomaPhoto" value="">&nbsp;缺照片&nbsp;&nbsp;
 						    <span style="float:right;">
 								<input class="button" type="button" id="btnStudentNeedDiplomaIssue" value="发放证书">
